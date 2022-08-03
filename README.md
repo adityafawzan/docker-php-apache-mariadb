1e1f3b52cf85f48b3a2139796f0a2f6e6f61ebe7
adityafawzan <fawzan89@gmail.com>
2022-08-04 00:02:45 +0700

    04-08-22-0002

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 - Please make sure, Docker Engine and Docker Compose already installed on your system
 - $ docker compose up -d
+- run phpMyAdmin at localhost:8000
+- run your App at localhost:8001/yourapp
--- public_html/index.php
+++ public_html/index.php
@@ -15,5 +15,5 @@ $conn = new mysqli($host, $user, $pass);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 } else {
-  echo "Connected to MySQL server successfully myDocker!";
+  echo "Connected to MySQL server successfully !";
 }
